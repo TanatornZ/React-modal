b812082047da0bcb81367e3f83acc65636f36f5c
BANKIEYZ\tanat <bank250145.bb@gmail.com>
2022-04-11 15:59:52 +0700

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
 
 function App() {
+
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={openModal} className='p-3 border rounded mt-10'>Click me!!!</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className='w-screen h-screen flex justify-center items-center '
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
     </div>
   );
 }
